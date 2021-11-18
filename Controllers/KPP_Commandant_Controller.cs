efcc7d8b080dfdfcfaf5bee710a2892d8bf93527
Zamaychik <91096338+jsfrau@users.noreply.github.com>
2021-11-18 11:23:49 +0300

    Update and rename KPP_Komendant_Controller.cs to KPP_Commandant_Controller.cs

--- Controllers/KPP_Commandant_Controller.cs
+++ Controllers/KPP_Commandant_Controller.cs
@@ -8,15 +8,15 @@ using Microsoft.Extensions.Logging;
 namespace Zamay.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("controller/commandant")]
     public class Komendant : ControllerBase
     {
-        [HttpGet("Settlement of students")]
+        [HttpGet("Settlement-of-students")]
         public string SettlementOfStudents(string str)
         {
             return str; // Метод для заселения студента
         }
-        [HttpGet("Issue a pass")]
+        [HttpGet("Issue-a-pass")]
         public string IssueAPass(string str)
         {
             return str; // Метод для выписания пропуска
